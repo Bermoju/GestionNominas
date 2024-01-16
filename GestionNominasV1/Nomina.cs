201f24ecb22b7a4053c7574434b0c995c800191c
Bermoju <bermudezmoralesjuan@hotmail.com>
2024-01-16 09:32:44 +0100

    Creación de la Propiedad NombreEmpleado

--- GestionNominasV1/Nomina.cs
+++ GestionNominasV1/Nomina.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,30 @@ namespace GestionNominasV1
         private string _puesto;
         private int _horas;
         private float _salarioHora;
+
+        // PROPIEDADES
+        /// <summary>
+        /// Nombre del Empleado
+        /// </summary>
+        public string NombreEmpleado
+        {
+            get
+            {
+                // Control de Contenido
+                if (string.IsNullOrEmpty(_nombre)) throw new Exception("Nombre del Trabajador no establecido");
+                // Devolución del valor
+                return _nombre;
+            }
+            set 
+            {
+                // Validación de los Datos a Establecer
+                if (string.IsNullOrEmpty(value)) throw new Exception("El nombre del trabajador no puedes ser null o la cadena vacía");
+                
+                // Establecimiento del valor al miembro privado
+                _nombre = value;
+
+            }
+        }
 
     }
 }
